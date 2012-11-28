aa3539b380c120d85ea2ba6356f4e3ca90d790af
Nicholas Pomponio <nicholas.pomponio1@marist.edu>
2012-11-28 09:34:29 -0500

    Revert "Project 5"
    This reverts commit c2b1c2f6e1b9c82a1a9cae5646fe1fa11ff25758.

--- functions.js
+++ functions.js
@@ -4,25 +4,8 @@ var position=0;
 var gun = false;
 var handcuffs = false;
 var keys = false;
-var inventory = new Array;
-var north = 0;
-var south = 1;
-var east  = 2;
-var west  = 3; 
-					//   N  S  E  W
-		var nav = [
-						[2, 1, 3, 4], //location 0
-						[0, -1,-2, -3], //location 1
-						[5, 0, 3, 4], //location 2
-						[9, -4, -5, 0], //location 3
-						[7, -6, 0, -7], //location 4
-						[-8, 2, 8, 6], //location 5
-						[-9, 7, 5, -10], //location 6
-						[6, 4, 2, -11], //location 7
-						[-12, 9, -13, 5], //location 8
-						[8, 3, -14, 2], //location 9
-					];
-					
+var inventory = "";
+
 function process()
 {
 	var command="";
@@ -30,165 +13,205 @@ function process()
 	command= command.toLowerCase();
 switch(command)
 	{
-		case "n" : col = 0;
+		case "n" : displayNorth();
 					break;
-		case "s" : col = 1;
+		case "s" : displaySouth();
 					break;
-		case "e" : col = 2;
+		case "e" : displayEast();
 					break;
-		case "w" : col = 3;
+		case "w" : displayWest();
 					break;
-		case "i": displayInventory();
-					break;			
-		case "score": displayScore();
-					break;
+		case "w" : displayWest();
+					break;	
 		case "help": displayHelp();
 					break;
 		case "take": takeItem();
 					break;
+		case "inventory": displayInventory();
+					break;
 		default: Error();
-		row = location;
-		return nav [row][col];
+}}
 		
-	}
-
-		row = position;
-		newLocale = nav[row][col];
-		position =newLocale;
-
-	if (position===0) {		
-		location0();		
+function displayNorth()
+	{
+	if (position===0) {
+		position=2;
+		location2();		
 	}	
-	else if (position===1) {		
-		location1();
-	}
-		else if (position===2) {
-		location2();
-	}
-		else if (position===3) {
-		location3();
-	}
-		else if (position===4) {
-		location4();
-	}
-		else if (position===5) {
+	else if (position===2) {
+		position=5;
 		location5();
 	}
-		else if (position===6) {
-		location6();
+		else if (position===5) {
+		position=5;
+		boundry();
 	}
-		else if (position===7) {
+		else if (position===1) {
+		position=0;
+		location0();
+	}
+		else if (position===3) {
+		position=9;
+		location9();
+	}
+		else if (position===4) {
+		position=7;
 		location7();
 	}
+		else if (position===6) {
+		position=6;
+		boundry();
+	}
+		else if (position===7) {
+		position=6;
+		location6();
+	}
 		else if (position===8) {
-		location8();
+		position=8;
+		boundry();
 	}
 		else if (position===9) {
-		location9();	
+		position=8;
+		location8();	
 	}
-		else if (position===-1)	{
-		position = 1;	
-		boundry();
-	}
-		else if (position===-2)	{
-		boundry();
-		position = 1;
-	}
-
-		else if (position===-3)	{
-		boundry();
-		position = 1;
-	}
-		else if (position===-4)	{
-		boundry();
-		position = 3;
-	}
-		else if (position===-5)	{
-		boundry();
-		position = 3;
-	}
-		else if (position===-6)	{
-		boundry();
-		position = 4;
-	}
-		else if (position===-7)	{
-		boundry();
-		position = 4;
-	}
-		else if (position===-8)	{
-		boundry();
-		position = 5;
-	}
-		else if (position===-9)	{
-		boundry();
-		position = 6;		
-	}
-		else if (position===-10)	{
-		boundry();
-		position = 6;
-		
-	}
-		else if (position===-11)	{
-		boundry();
-		position = 7;
-		
-	}
-		else if (position===-12)	{
-		boundry();
-		position = 8;
-			
-	}
-		else if (position===-13)	{
-		boundry();
-		position = 8;
-		
-	}
-		else if (position===-14)	{
-		boundry();
-		position = 9;
-		
-	}	
-	//clears the textbox.
-		var inputTxtBox = document.getElementById('txtCommand');
-		inputTxtBox.value = "";
-
 	}
 	
-	function inputTxt(newTxt)
+		
+function displaySouth()
 	{
-		var inputTxtBox = document.getElementById("txtCommand");
-		inputTxtBox.value = newTxt + inputTxtBox.value;
+     	if (position===0) {
+		position=1;
+		location1();		
+	}	
+	else if (position===1) {
+		position=1;
+		boundry();
+	}
+		else if (position===2) {
+		position=0;
+		location0();
+	}
+		else if (position===3) {
+		position=3;
+		boundry();
+	}
+		else if (position===4) {
+		position=4;
+		boundry();
+	}
+		else if (position===5) {
+		position=2;
+		location2();
+	}	
+		else if (position===6) {
+		position=7;
+		location7();
+	}
+		else if (position===7) {
+		position=4;
+		location4();
+	}
+		else if (position===8) {
+		position=9;
+		location9();
+	}
+		else if (position===9) {
+		position=3;
+		location3();
 	}
 	
+	}
 		
-function travelNorth()
-{
-	var message = "n";
-	inputTxt (message);
-	process();
-}
-
-function travelSouth()
-{
-	var message = "s";
-	inputTxt (message);
-	process();
-}
-
-function travelEast()
-{
-	var message = "e";
-	inputTxt (message);
-	process();
-}
-
-function travelWest()
-{
-	var message = "w";
-	inputTxt (message);
-	process();
-}
-
+function displayEast()
+	{
+		if (position===0) {
+		position=3;
+		location3();
+		}
+		else if (position===1) {
+		position=1;
+		boundry();
+		
+		}
+		else if (position===2) {
+		position=9;
+		location9();
+		}
+		else if (position===5) {
+		position=8;
+		location8();
+		}
+		else if (position===4) {
+		position=0;
+		location0();
+		}
+		else if (position===3) {
+		position=3;
+		boundry();
+		}
+		else if (position===6) {
+		position=5;
+		location5();
+	}
+		else if (position===7) {
+		position=2;
+		location2();
+	}
+		else if (position===8) {
+		position=8;
+		boundry();
+	}
+		else if (position===9) {
+		position=9;
+		boundry();
+	}
+	}
+	
+function displayWest()
+	{
+		if (position===0) {
+		position=4;
+		location4();
+		}
+		else if (position===1) {
+		position=1;
+		boundry();
+		}
+		else if (position===2) {
+		position=7;
+		location7();
+		
+		}
+		else if (position===3) {
+		position=0;
+		location0();
+		}
+		else if (position===4) {
+		position=4;
+		boundry();
+		}
+		else if (position===5) {
+		position=6;
+		location6();
+		
+		}		
+		else if (position===6) {
+		position=6;
+		boundry();
+	}
+		else if (position===7) {
+		position=7;
+		location0();
+	}
+		else if (position===8) {
+		position=5;
+		location5();
+	}
+		else if (position===9) {
+		position=2;
+		location2();
+	}
+	}
 	
 function takeItem()
 	{
@@ -196,24 +219,24 @@ function takeItem()
 	   if(!gun)
 	   {
 		gun = true;
-		inventory[0] = "gun, ";
-		updateDisplay(location[1].item.description);
+		inventory = inventory + "gun, ";
+		updateDisplay("You take the gun from him");
 	   }
 	} 
 	else if (position===4) {
 	   if(!keys)
 	   {
 		keys = true;
-		inventory[0] = "keys, ";
-		updateDisplay(location[4].item.description);
+		inventory = inventory + "keys, ";
+		updateDisplay("You pick up the keys");
 	   }
 	} 
 	else if (position===7) {
 	   if(!handcuffs)
 	   {
 		handcuffs = true;
-		inventory[0] = "handcuffs, ";
-		updateDisplay(location[7].item.description);
+		inventory = inventory + "handcuffs, ";
+		updateDisplay("You pick up the handcuffs");
 	   }
 	}  	
 	 
@@ -250,8 +273,8 @@ function displayHelp()
 }
 function displayInventory()
 {
-	
-				updateDisplay(inventory);
+		var message = inventory;
+				updateDisplay(message);
 }				
 function updateDisplay(updatedText)
 {
--- locations.js
+++ locations.js
@@ -1,121 +1,3 @@
-var Locations = new Array();
-var items = new Array();
-
-function Item()
-{
-	this.id = -1;
-	this.name="";
-	this.description = "";
-	this.toString = function()
-	{
-	 return		"Id: " + this.id + "' Name: "+this.name + ", Description: \"" + this.description + "\"";
-	}
-}
-
-//Location Prototype
-function Locale()
-{
-  this.id = -1;
-  this.name = "";
-  this.description = "";
-  this.item = "";
-  this.toString = function ()
-  {
-	return"Id:"+this.id+", Name: "+this.name+", Description:\""+this.description+"\", Item:"+this.item.toString();
-  }
-  
- }
- //fill in location prototype
- 
- 
-   location[0] = new Locale();
-   location[0].id= 0;
-   location[0].name = "Precinct";
-   location[0].description = "You are back at the precinct";
-   location[0].item = "nothing";
-   
-   
-   location[1] = new Locale();
-   location[1].id= 1;
-   location[1].name = "South Side of New York City";
-   location[1].description = "You have arrived at the south side of New York City.  You have caught a person jaywaking at 3:30 AM. The jaywalker has an illegal firearm on him";
-   location[1].item = "gun";
-   
-   
-   location[2] = new Locale();
-   location[2].id= 2;
-   location[2].name = "North side of New York City";
-   location[2].description = "You have arrived at the north side of New York City.  You have caught a robber at a local deli.";
-   location[2].item = "nothing";
-   
-   
-   location[3] = new Locale();
-   location[3].id= 3;
-   location[3].name = "East side of New York City";
-   location[3].description = "You have arrived at the east side of New York City.  You have pulled over a twenty-two year old male and found 100 dollars worth of drugs in his trunk.";
-   location[3].item = "nothing";
-   
-   
-   location[4] = new Locale();
-   location[4].id= 4;
-   location[4].name = "West side of New York City";
-   location[4].description = "You have arrived at the west side of New York City.  You have caught a theif stealing a purse from an elderly woman. Her keys are still on the sidewalk.";
-   location[4].item = "keys";
-   
-   
-   location[5] = new Locale();
-   location[5].id= 5;
-   location[5].name = "The Bay";
-   location[5].description = "You have arrived by the bay and caught a person with a body bag in the trunk of his car";
-   location[5].item = "nothing";
-   
-   
-   location[6] = new Locale();
-   location[6].id= 6;
-   location[6].name = "Jewlery Store";
-   location[6].description = "You have caught a robber at a local jewlery store.";
-   location[6].item = "nothing";
-   
-   
-   location[7] = new Locale();
-   location[7].id= 7;
-   location[7].name = "Apartment";
-   location[7].description = "You have caught a guy beating his wife in an apartment. There are hancuffs on the ground.";
-   location[7].item = "handcuffs";
-   
-   
-   location[8] = new Locale();
-   location[8].id= 8;
-   location[8].name = "Yankee Stadium";
-   location[8].description = "You have caught a person soliciting outside of Yankee stadium.";
-   location[8].item = "nothing";
-   
-   
-   location[9] = new Locale();
-   location[9].id= 0;
-   location[9].name = "Gas Station";
-   location[9].description = "You have caught a person trying to rob a local gas station.";
-   location[9].item = "nothing";
-   
-//Item Prototype  
-   location[1].item = new Item();
-   location[1].item.id= 0;
-   location[1].item.name = "Gun";
-   location[1].item.description = "You take the gun from him.";
-   
-   
-   location[4].item = new Item();
-   location[4].item.id= 0;
-   location[4].item.name = "Keys";
-   location[4].item.description = "You pick up the keys.";
-   
-   
-   location[7].item = new Item();
-   location[7].item.id= 0;
-   location[7].item.name = "Handcuffs";
-   location[7].item.description = "You pick up the handcuffs.";
-   
-   
 function startingLocation()
 {
 	var message= "You are a police officer in the middle of New York City.  You work night hours so this is when the most robberies and crimes occur.  Your objective is to catch as many robbers and criminals as possible throughout the night.  You are currently at your precinct awaiting to catch criminals throughout the night.";
@@ -123,7 +5,8 @@ function startingLocation()
 }
 function location0()
 	{
-		updateDisplay(location[0].description);
+		var message = "You are back at the precinct";
+		updateDisplay(message);
 		
 		   enableSouth();
 		   enableEast();
@@ -139,7 +22,16 @@ function location1()
 		   local1 = local1+1;
 
 		}
-			updateDisplay(location[1].description);
+			if(!gun)
+			{	
+				var message = "You have arrived at the south side of New York City.  You have caught a person jaywaking at 3:30 AM. The jaywalker has an illegal firearm on him";
+				updateDisplay(message);
+			}
+			else if(gun ===true)
+			{
+				var message = "You have arrived at the south side of New York City." ;
+				updateDisplay(message);	
+			}
 	 	   disableSouth();
 		   disableEast();
 		   disableWest();
@@ -153,7 +45,8 @@ function location2()
 		   addScore();
 		   local2 = local2+1;
 		}
-			updateDisplay(location[2].description);
+		var message = "You have arrived at the north side of New York City.  You have caught a robber at a local deli.";
+		updateDisplay(message);
 		
 			enableSouth();
 		   enableEast();
@@ -167,7 +60,8 @@ function location3()
 		   addScore();
 		   local3 = local3+1;
 		}
-			updateDisplay(location[3].description);
+		var message = "You have arrived at the east side of New York City.  You have pulled over a twenty-two year old male and found 100 dollars worth of drugs in his trunk.";
+		updateDisplay(message);
 		   disableSouth();
 		   disableEast();
 		   enableWest();
@@ -180,8 +74,16 @@ function location4()
 		   addScore();
 		   local4 = local4+1;
 		}
-			updateDisplay(location[4].description);
-			
+			if(!keys)
+			{
+				var message = "You have arrived at the west side of New York City.  You have caught a theif stealing a purse from an elderly woman. Her keys are still on the sidewalk";
+				updateDisplay(message);
+			}
+			else if(keys === true)
+			{
+				var message = "You have arrived at the west side of New York City.";
+				updateDisplay(message);
+			}
 		   disableSouth();
 		   enableEast();
 		   disableWest();
@@ -194,7 +96,8 @@ function location5()
 		   addScore();
 		   local5 = local5+1;
 		}
-			updateDisplay(location[5].description);
+		var message = "You have arrived by the bay and caught a person with a body bag in the trunk of his car.";
+		updateDisplay(message);
 			enableSouth();
 		   enableEast();
 		   enableWest();
@@ -207,7 +110,8 @@ function location5()
 		   addScore();
 		   local6 = local6+1;
 		}
-			updateDisplay(location[6].description);
+		var message = "You have caught a robber at a local jewlery store.";
+		updateDisplay(message);
 		
 			enableSouth();
 		   enableEast();
@@ -221,8 +125,16 @@ function location7()
 		   addScore();
 		   local7 = local7+1;
 		}
-			updateDisplay(location[7].description);
-			
+			if (!handcuffs)
+			{
+				var message = "You have caught a guy beating his wife in an apartment. There are hancuffs on the ground.";
+				updateDisplay(message);
+			}
+			else if (handcuffs===true)
+			{
+				var message = "You are back at the apartment.";
+				updateDisplay(message);	
+			}
 			enableSouth();
 		   enableEast();
 		   disableWest();
@@ -235,7 +147,8 @@ function location8()
 		   addScore();
 		   local8 = local8+1;
 		}	
-			updateDisplay(location[8].description);
+		var message = "You have caught a person soliciting outside of Yankee stadium.";
+		updateDisplay(message);
 		   enableSouth();
 		   disableEast();
 		   enableWest();
@@ -248,7 +161,8 @@ function location9()
 		   addScore();
 		   local9 = local9+1;
 		}
-			updateDisplay(location[9].description);
+		var message = "You have caught a person trying to rob a local gas station.";
+		updateDisplay(message);
 		   enableSouth();
 		   disableEast();
 		   enableWest();
